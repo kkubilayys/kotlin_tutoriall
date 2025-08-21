14f3d079bdea813ec32b3dd6c72564da13fb269d
kkubilayys <kayirkan@icloud.com>
2025-08-21 12:10:08 +0300

    ADD: onclick function for button

---
+++ app/src/main/java/com/example/kahvecim/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.kahvecim
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kahvecim.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+
+        binding.benimbutonum.setOnClickListener {
+            binding.benimtextim.text = "Hello World!"
+        }
+
+
+
+    }
+
+
+}
